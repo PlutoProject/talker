6d6069f7bfdbcb12e714411b9e3273188f446d3d
nostalfinals <yuu8583@proton.me>
2023-08-02 21:53:15 +0800

    build: add missed modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ allprojects {
     }
 }
 
-val apis = listOf("talker-api", "talker-api-client", "talker-api-server")
+val apis = listOf("talker-api", "talker-api-client", "talker-api-server", "talker-bukkit", "talker-velocity", "talker-bungee")
 
 val repoUser: String = System.getenv("TALKER_MAVEN_USER")
 val repoPwd: String = System.getenv("TALKER_MAVEN_PASSWORD")
